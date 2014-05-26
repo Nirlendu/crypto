1b8033a023a86e76147a3002f94be6593eed1ac8
vickianand <vicki_anand@hotmail.com>
2014-05-26 19:22:24 +0530

    some changes in Makefile

--- src/AES/C_wrap/test_main.c
+++ src/AES/C_wrap/test_main.c
@@ -1,13 +1,19 @@
 
 #include <stdio.h>
-#include "../include/rijndael1.h"
+#include "../include/rijndael.h"
 
 int main()
 {
-  char * password = "vickianandyadav";
-  char * message = "This is some text, which is to be encrypted. Do work  man. please... please.... please.";
-  char * cipher_text = enc(password, message);
-  printf("\n%s\n\n", cipher_text);
-  char * plain_text =  dec(password, cipher_text);
-  printf("%s\n", plain_text);
+  char * password1 = "vickianandyadav";
+  char * password2 = "rohitanurag2";
+  char * message1 = "This is some text, which is to be encrypted. Do work  man. please... please.... please.";
+  char * message2 = "This is a different text. Lets see what hhappens to this";
+  char * cipher_text1 = enc(password1, message1);
+  char * cipher_text2 = enc(password2, message2);
+  printf("\n%s\n", cipher_text1);
+  printf("%s\n", cipher_text2);
+  char * plain_text1 = dec(password1, cipher_text1);
+  char * plain_text2 = dec(password2, cipher_text2);
+  printf("%s\n", plain_text1);
+  printf("%s\n", plain_text2);
 }
--- src/AES/lib/rijndael.c
+++ src/AES/lib/rijndael.c
@@ -16,6 +16,8 @@
 
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "../include/base64.h"
 
 typedef unsigned long u32;
